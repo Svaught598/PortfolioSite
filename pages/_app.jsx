f6075e7218117d3835c3959d4591e65f5b0d2f21
Steven Vaught <svaught598@gmail.com>
2021-07-09 17:12:47 -0700

    darkMode persists on app close now

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ParticleBackground from '../components/ui/ParticleBackground.jsx'
+import { str2bool } from 'utils';
 
 import '../styles/globals.css'
-// import 'bootstrap/dist/css/bootstrap.css'
 import "tailwindcss/tailwind.css"
 import 'animate.css'
 
 function MyApp({ Component, pageProps }) {
   const [darkBackground, setDarkBackground] = useState(false)
-
   pageProps.darkBackground = darkBackground
   pageProps.setDarkBackground = setDarkBackground
 
+  useEffect(() => {
+    const darkMode = localStorage.getItem('darkMode');
+    if (darkMode != null)
+      setDarkBackground(str2bool(darkMode))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkBackground)
+  }, [darkBackground])
+
   return (
     <>
-      <Component { ...pageProps } />
-      <ParticleBackground darkMode={ darkBackground } />
+      <Component {...pageProps} />
+      <ParticleBackground darkMode={darkBackground} />
     </>
   )
 }
