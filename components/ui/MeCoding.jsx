7caae4c7cbe020b0f9528783a86febe3b235e1dc
Steven Vaught <svaught598@gmail.com>
2023-01-28 23:19:15 -0700

    fix next image problem

--- components/ui/MeCoding.jsx
+++ components/ui/MeCoding.jsx
@@ -14,7 +14,11 @@ export default function MeCoding() {
       <div className='bg-transparent p-4 lg:p-0 absolute top-0 left-0 w-full h-full rounded-full border-16 border-charcoal z-50' style={{ transform: 'translateZ(20px)'}} ></div>
       <div className='bg-transparent p-4 lg:p-0 absolute top-0 left-0 w-full h-full rounded-full border-16 border-charcoal z-50' style={{ transform: 'translateZ(30px)'}} ></div>
       <div className='bg-transparent p-4 lg:p-0 absolute top-0 left-0 w-full h-full rounded-full border-16 border-charcoal z-50' style={{ transform: 'translateZ(40px)'}} ></div>
-      <Image className='object-cover absolute top-0 rounded-full' src='/images/me-coding-bg.svg' layout='fill' />
+      <img 
+        className="absolute top-0 bottom-0 left-0 right-0 rounded-full object-cover h-full w-full"
+        src={ require('../../public/images/me-coding-bg.svg') }
+      />
+      {/* <Image className='object-cover absolute top-0 rounded-full' src='/images/me-coding-bg.svg' layout='fill' /> */}
       <div className='flex flex-row justify-center items-center ' style={{ transformStyle: 'flat' }}>
         <div ref={ref} id="pixel-art" className="anime grid grid-cols-64 opacity-0" >
           { meCoding.map((pixel,i) => <div key={i} className="w-1 h-1 md:w-2 md:h-2 el" style={{ backgroundColor: pixel, marginRight: "1px", marginBottom: "1px" }} />)}
